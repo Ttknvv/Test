1e9ebc769cac7129179baf3ad9082bd52dd36eb4
Vova <vvvova24@gamil.com>
2022-09-10 12:47:38 +0700

    v1.0

--- Program.cs
+++ Program.cs
@@ -1,18 +1,4 @@
-﻿int CountStrings(string[] Mass)
-{
-    int length = Mass.Length;
-    int count = 0;
-    for (int i = 0; i < length; i++)
-    {
-        if (Mass[i].Length < 4)
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-void PrintMass(string[] Mass)
+﻿void PrintMass(string[] Mass)
 {
     int length = Mass.Length;
     for (int i = 0; i < length; i++)
@@ -24,20 +10,30 @@ void PrintMass(string[] Mass)
 void Test()
 {
     string[] Mass = new string[] {"hello", "2", "world", ":-)"}; //Проверяемый массив
-    string[] Mass2 = new string[CountStrings(Mass)];
     int length = Mass.Length;
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (Mass[i].Length < 4)
+        {
+            count++;
+        }
+    }
+
+    string[] Mass1 = new string[count];
     int ind = 0;
 
     for (int i = 0; i < length; i++)
     {
         if (Mass[i].Length < 4)
         {
-            Mass2[ind] = Mass[i];
+            Mass1[ind] = Mass[i];
             ind++;
         }
     }
 
-    PrintMass(Mass2);
+    PrintMass(Mass1);
 }
 
 Test();
